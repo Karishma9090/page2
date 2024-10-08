2ec64d09c8760b3f5b08344d1f37145ef5ca1d3f
karishma pradhan <karishmapradhan9090@gmail.com>
2024-10-08 11:04:59 +0530

    8.10.24commit

--- app/src/main/java/com/example/page/page2.java
+++ app/src/main/java/com/example/page/page2.java
@@ -1,6 +1,9 @@
 package com.example.page;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class page2 extends AppCompatActivity {
+    Button Backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_page2);
+        Backbtn=findViewById(R.id.back);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+        Backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(page2.this, MainActivity.class));
+            }
         });
     }
 }
