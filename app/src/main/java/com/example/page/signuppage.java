8e0e44aac51b2f611fce7bc1a7b3d6432e19dde4
karishma pradhan <karishmapradhan9090@gmail.com>
2024-10-08 10:55:30 +0530

    8.10.24commit

---
+++ app/src/main/java/com/example/page/signuppage.java
@@ -0,0 +1,37 @@
+package com.example.page;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class signuppage extends AppCompatActivity {
+    Button signupbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signuppage);
+        signupbtn=findViewById(R.id.btn);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signuppage.this, page2.class));
+            }
+        });
+
+    }
+}
